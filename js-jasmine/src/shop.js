194458bcff501177c204f39707961ffe7ab8bbdf
dearshrewdwit <dearshrewdwit@gmail.com>
2021-07-06 16:47:12 +0100

    update methods

--- js-jasmine/src/shop.js
+++ js-jasmine/src/shop.js
@@ -4,18 +4,14 @@ class Shop {
   }
 
   updateBackstagePass(item) {
-    if ( item.quality < 50 ) {
-      item.quality++
-      if (item.sellIn < 11 && item.quality < 50) { item.quality++ }
-      if (item.sellIn < 6 && item.quality < 50) { item.quality++ }
-    }
+    if (item.quality < 50) { item.quality++ }
+    if (item.sellIn < 11 && item.quality < 50) { item.quality++ }
+    if (item.sellIn < 6 && item.quality < 50) { item.quality++ }
   }
 
   updateAgedBrie(item) {
-    if ( item.quality < 50 ) {
-      item.quality++
-      if ( item.sellIn < 0 ) { item.quality++ }
-    }
+    if (item.quality < 50) { item.quality++ }
+    if ( item.sellIn < 0 && item.quality < 50) { item.quality++ }
   }
 
   updateQuality() {
