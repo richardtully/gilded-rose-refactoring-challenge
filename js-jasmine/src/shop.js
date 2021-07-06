6d7611385af9636fb40643feef50c2a60978c29d
dearshrewdwit <dearshrewdwit@gmail.com>
2021-07-06 16:58:14 +0100

    complete extracting methods for sulfuras, backstage passes and aged brie updates

--- js-jasmine/src/shop.js
+++ js-jasmine/src/shop.js
@@ -4,37 +4,31 @@ class Shop {
   }
 
   updateBackstagePass(item) {
+    item.sellIn--
     if (item.quality < 50) { item.quality++ }
     if (item.sellIn < 11 && item.quality < 50) { item.quality++ }
     if (item.sellIn < 6 && item.quality < 50) { item.quality++ }
+    if (item.sellIn < 0) { item.quality = 0 }
   }
 
   updateAgedBrie(item) {
+    item.sellIn--
     if (item.quality < 50) { item.quality++ }
     if ( item.sellIn < 0 && item.quality < 50) { item.quality++ }
   }
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+      if (this.items[i].name === 'Aged Brie') { this.updateAgedBrie(this.items[i]) }
+      if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') { this.updateBackstagePass(this.items[i]) }
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          this.items[i].quality = this.items[i].quality - 1;
         }
         this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (this.items[i].name === 'Aged Brie') { this.updateAgedBrie(this.items[i]) }
-        if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') { this.updateBackstagePass(this.items[i]) }
-
         if (this.items[i].sellIn < 0) {
-          if (this.items[i].name != 'Aged Brie') {
-            if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-              if (this.items[i].quality > 0) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            } else {
-              this.items[i].quality = this.items[i].quality - this.items[i].quality;
-            }
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       }
